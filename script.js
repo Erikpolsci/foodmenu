0c4f72e6226c9778586c49dfe7c62da17b93d825
Erik Sales <eriksales1@hotmail.com>
2023-01-20 10:22:44 -0500

    MRP

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector(".menu-itens")
 
 const menuItem = [{
-    
+    FoodName: "Avocado Toast",
+    foodImg: "images/avocado-g64796c424_640.jpg",
+    price: "$6",
+    foodType: "breakfast",
+    description:"loren..."
 }]
